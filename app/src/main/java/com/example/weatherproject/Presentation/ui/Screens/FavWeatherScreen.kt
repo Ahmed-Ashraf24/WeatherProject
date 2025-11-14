30379272b9a2e7dfbce5e18684a5f5830e86c963
Ahmed Ashraf <ahmedashrafahmed2716@gmail.com>
2025-11-14 17:14:18 +0200

    feat:adding label to favorite list

--- app/src/main/java/com/example/weatherproject/Presentation/ui/Screens/FavWeatherScreen.kt
+++ app/src/main/java/com/example/weatherproject/Presentation/ui/Screens/FavWeatherScreen.kt
@@ -3,22 +3,29 @@ package com.example.weatherproject.Presentation.ui.Screens
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.weatherproject.Presentation.UIModel.toFavWeather
 import com.example.weatherproject.Presentation.UIModel.toUiFavWeather
 import com.example.weatherproject.Presentation.ViewModel.WeatherViewModel
 import com.example.weatherproject.Presentation.ui.Component.FavWeatherItem
+import com.example.weatherproject.Presentation.ui.theme.Typography
 import com.example.weatherproject.Presentation.ui.theme.colorPalList
 
 @RequiresApi(Build.VERSION_CODES.O)
@@ -47,6 +54,11 @@ fun FavWeatherScreen(
                 .fillMaxSize()
                 .padding(vertical = 10.dp)
         ) {
+            item{
+                Row (Modifier.fillMaxWidth().padding(vertical =10.dp ), horizontalArrangement = Arrangement.Center){
+                    Text("Favorite List", style = Typography.labelLarge.copy(color = Color.White, fontSize = 40.sp))
+                }
+            }
             favorites?.let{
                 items(it.size) { index ->
                     val fav = it[index]
